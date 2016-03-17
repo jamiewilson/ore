f7b37df3afd4c0eeef94a8eadad58fa7f27cce97
Jamie Wilson <jamie@jamiewilson.io>
2016-03-17 17:09:16 -0500

    remove deploy scripts, and chalk package

--- config/serve.js
+++ config/serve.js
@@ -1,5 +1,4 @@
-//  Start, Steps, and Build Error Messages
-var chalk = require('chalk');
+//  Serve and report build errors
 var browsersync = require('browser-sync').create();
 var config = { browsersync: require('./browsersync') };
 
@@ -7,8 +6,7 @@ module.exports = function(error) {
   browsersync.init(config.browsersync);
   if (error) {
     // Reports any build errors on initial build
-    // TODO: throw after a rebuild as well
-    console.log(chalk.magenta.underline('\nOops, there was a problem!'));
+    console.log('\nOops, there was a problem!');
     console.log(error.message);
   }
 }
